351c8f03acdda369ffef09c6aa38a046d161c418
Adam Larson <al762@nau.edu>
2022-04-28 16:24:43 -0700

    Initialize Spring API

---
+++ TechDemo/src/main/java/com/WhatsUpDoc/TechDemo/TechDemoApplication.java
@@ -0,0 +1,13 @@
+package com.WhatsUpDoc.TechDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechDemoApplication.class, args);
+	}
+
+}
