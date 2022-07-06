094fdd7920d1adff122c1d4072e4b3848506e426
Adam Larson <al762@nau.edu>
2022-07-06 14:22:05 -0700

    Initialize REST API (Fall2022)

---
+++ PhDTracker/src/main/java/com/WhatsUpDoc/PhDTracker/Users/Student/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.WhatsUpDoc.PhDTracker.Users.Student;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, String> {
+}
---
+++ PhDTracker/src/test/java/com/WhatsUpDoc/PhDTracker/PhDTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WhatsUpDoc.PhDTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhDTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
