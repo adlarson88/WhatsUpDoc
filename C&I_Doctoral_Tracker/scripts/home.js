3155e4521aed29949568acf1eee7cd7cb51c7efe
eml292 <90654466+eml292@users.noreply.github.com>
2022-10-18 09:00:59 -0700

    Updated home.js
    Dropdowns now clear before new ones are dropped. Each phase now defaults to task 1 of that phase when opened

--- C&I_Doctoral_Tracker/scripts/home.js
+++ C&I_Doctoral_Tracker/scripts/home.js
@@ -5,46 +5,31 @@ function dropDown(doc)
     var taskDoc;
     for (index = 0; index < target.length; index++) {
       if (!target[index].classList.contains('fileView')) {
-        if (target[index].style.display === "none") {
-            target[index].style.display = "block";
-        } else {
-          target[index].style.display = "none";
-        }
+
+        target[index].style.display = "block";
+
       }
       else if(doc.includes('task')){
-        if (target[index].style.display === "none") {
-            target[index].style.display = "block";
-        } else {
-          target[index].style.display = "none";
-        }
+
+        target[index].style.display = "block";
 
       }
     }
     if (!doc.includes('task')) {
       target = document.getElementById(doc);
-      if (target.style.fontWeight === "bold") {
-          target.style.fontWeight = "normal";
-          target.style.borderStyle = 'none';
-          target.style.borderTopStyle = 'solid';
-          target.style.borderWidth = '0px';
-        } else {
-          target.style.fontWeight = "bold";
-          target.style.borderStyle = 'solid';
-          target.style.borderWidth = '2px';
-        }
+
+      target.style.fontWeight = "bold";
+      target.style.borderStyle = 'solid';
+      target.style.borderWidth = '.4vw';
+
     } else {
       taskDoc = doc.split(" ");
       target = document.getElementById(taskDoc[1]);
-      if (target.style.fontWeight === "bold") {
-          target.style.fontWeight = "normal";
-          target.style.borderStyle = 'none';
-          target.style.borderTopStyle = 'solid';
-          target.style.borderWidth = '0px';
-        } else {
-          target.style.fontWeight = "bold"; 
-          target.style.borderStyle = 'solid';
-          target.style.borderWidth = '2px';
-        }
+
+      target.style.fontWeight = "bold"; 
+      target.style.borderStyle = 'solid';
+      target.style.borderWidth = '.4vw';
+
     }
 }
 
@@ -113,4 +98,41 @@ function upload(doc)
 {
 
   checkProgress(doc);
+}
+
+function hideClass(doc)
+{
+  var target = document.getElementsByClassName(doc);
+  var index;
+
+  if(doc.includes('milestoneDataText')){
+    hideClass('fileView');
+    target = document.getElementsByClassName('milestoneDataText');
+    for (index = 0; index < target.length; index++) {
+      target[index].style.fontWeight = "normal";
+      target[index].style.borderStyle = 'none';
+      target[index].style.borderTopStyle = 'solid';
+      target[index].style.borderWidth = '0.2vw';
+    }
+  }
+  else if(doc.includes('milestoneData')){
+    target = document.getElementsByClassName('milestoneData');
+    for (index = 0; index < target.length; index++) {
+      target[index].style.display = "none";
+    }
+  }
+  else if(doc.includes('milestone')){
+    hideClass('milestoneData');
+    target = document.getElementsByClassName('milestone');
+    for (index = 0; index < target.length; index++) {
+      target[index].style.fontWeight = "normal";
+      target[index].style.borderWidth = '0.2vw';
+    }
+  }
+  if(doc.includes('fileView')){
+    target = document.getElementsByClassName('fileView');
+    for (index = 0; index < target.length; index++) {
+      target[index].style.display = "none";
+    }
+  }
 }
