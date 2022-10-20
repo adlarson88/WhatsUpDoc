e2ddc145def0626031710e9d9c6236ac95343dc7
eml292 <90654466+eml292@users.noreply.github.com>
2022-10-19 23:47:44 -0700

    Delete emergency.js
    Unnecessary file from early demo

--- C&I_Doctoral_Tracker/scripts/emergency.js
+++
@@ -1,9 +0,0 @@
-function dropDown(doc) {
-    var target = document.getElementById(doc);
-    if (target.style.display === "none") {
-        target.style.display = "block";
-    } else {
-        target.style.display = "none";
-    }
-    document.getElementById('one').style.backgroundColor = "#41F415";
-}
