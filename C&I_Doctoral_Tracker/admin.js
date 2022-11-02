b6fd924238ecf33397f6119aae9ef63f6b9e0f99
brandonbjs <68050507+brandonbjs@users.noreply.github.com>
2022-11-01 20:56:06 -0700

    Added files to correct directory

--- C&I_Doctoral_Tracker/admin.js
+++ C&I_Doctoral_Tracker/admin.js
@@ -1,55 +1,47 @@
-$(document).ready(function () {
-    $('#studentTable').DataTable();
-  });
+// page onload function
+function preparePage() {
 
-// Function to embed student home pages after search has been made
-function embedHomePage(form) {
-    if (document.getElementById('searchBarResponse') == "bjs397")
-        {
-            form.action = "https://ceias.nau.edu/capstone/projects/CS/2022/WhatsUpDoc_S22/C&I_Doctoral_Tracker/home.html"
-            return true;
-        }
-    else 
-        {
-            alert(form.action);
-            return false;
-        }
+    var tableClicked = document.getElementById("adminStudentControlsWindow");
+    var tableUnclicked = document.getElementById("adminPhaseReviewWindow");
+
+    tableClicked.style.display = "block";
+    tableUnclicked.style.display = "none";
 }
+
+
+
 
 // Function that opens the admin student control window upon navigation button click
 function openAdminStudentControlsWindow() {
-    var buttonClicked = document.getElementById("adminStudentControlsWindow");
-    var buttonUnclicked = document.getElementById("searchWindow");
-    if (buttonClicked.style.display === "none" && buttonUnclicked.style.display === "none") 
+    
+    var tableClicked = document.getElementById("adminStudentControlsWindow");
+    var tableUnclicked = document.getElementById("adminPhaseReviewWindow");
+
+    if (tableClicked.style.display === "none" && tableUnclicked.style.display === "block" )
         {
-            buttonClicked.style.display = "block";
-        } 
-    else if(buttonClicked.style.display === "none" && buttonUnclicked.style.display != "none")
-        {
-            buttonClicked.style.display = "block";
-            buttonUnclicked.style.display = "none";
-        }
-    else
-        {
-            buttonClicked.style.display = "none";
+            tableClicked.style.display = "block";
+            tableUnclicked.style.display = "none";
         }
 }
 
   // Function that opens the admin student control window upon navigation button click
-function openSearchWindow() {
-    var buttonClicked = document.getElementById("searchWindow");
-    var buttonUnclicked = document.getElementById("adminStudentControlsWindow");
-    if (buttonClicked.style.display === "none" && buttonUnclicked.style.display === "none") 
+function openPhaseReviewWindow() {
+    
+    var tableClicked = document.getElementById("adminPhaseReviewWindow");
+    var tableUnclicked = document.getElementById("adminStudentControlsWindow");
+
+    if (tableClicked.style.display === "none" && tableUnclicked.style.display === "block" )
         {
-            buttonClicked.style.display = "block";
-        } 
-    else if(buttonClicked.style.display === "none" && buttonUnclicked.style.display != "none")
-        {
-            buttonClicked.style.display = "block";
-            buttonUnclicked.style.display = "none";
+            tableClicked.style.display = "block";
+            tableUnclicked.style.display = "none";
         }
-    else
-        {
-            buttonClicked.style.display = "none";
-        }
+}
+
+// configure google sign-out button
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      window.location.href = "https://ceias.nau.edu/capstone/projects/CS/2022/WhatsUpDoc_S22/C&I_Doctoral_Tracker/index.html";
+      console.log('User signed out.');
+    });
 }
