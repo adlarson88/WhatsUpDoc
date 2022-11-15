1d2b968fe03f147d38607e2e90763a057edacffc
brandonbjs <68050507+brandonbjs@users.noreply.github.com>
2022-11-15 10:34:30 -0700

    Correctly populates Student Table with DB data
    Used JavaScript magic to contact DB, save data as JSON and correctly map it to our Student Table. Also was able to get CSV, Excel, and Print button working. Will work to get CRUD buttons functional ASAP now that data is being correctly sourced.

--- C&I_Doctoral_Tracker/scripts/admin.js
+++ C&I_Doctoral_Tracker/scripts/admin.js
@@ -1,5 +1,9 @@
+// Editor/DataTables stuff
+var userIDdata = 'eml292'; // temp userID
+var completeList;
+
 // page onload function
-function preparePage() {
+async function preparePage() {
 
     var tableClicked = document.getElementById("adminStudentControlsWindow");
     var tableUnclicked = document.getElementById("adminPhaseReviewWindow");
@@ -8,6 +12,45 @@ function preparePage() {
     tableClicked.style.display = "block";
     tableUnclicked.style.display = "none";
     embeddedWindowUnclicked.style.display = "none";
+
+    const downloadRequest = 'https://doctracker.org:8443/user/all';
+    const downloadOptions = {
+    
+        headers: {'Content-Type' : 'application/json'},
+    } ;
+    
+    const request = new Request(downloadRequest, downloadOptions);
+
+    const response = await fetch(request);
+
+    completeList = await response.text();
+
+    console.log(completeList);
+
+    completeList = JSON.parse(completeList);
+    
+
+    $(document).ready( function () {
+        var studentTable = $('#adminStudentControlsTable').DataTable( {
+            data: completeList,
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'excel', 'pdf'
+            ],
+            columns: [
+                { data: 'userID' },
+                { data: 'last_name' },
+                { data: 'first_name' },
+                { data: 'advisor' },
+                { data: 'term_activation' },
+                { data: 'admin' },
+            ]
+        } );
+
+        studentTable.buttons().container()
+        .appendTo( $('.col-sm-6:eq(0)', studentTable.table().container() ) );
+    } );
+
 }
 
 
